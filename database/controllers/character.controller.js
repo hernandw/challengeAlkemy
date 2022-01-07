7d27c2f5de983d7e407646043eb42a89356114b5
hernandw <hernandw@gmail.com>
2022-01-06 21:27:47 -0300

    delete, update de character

--- database/controllers/character.controller.js
+++ database/controllers/character.controller.js
@@ -1,9 +1,120 @@
+const res = require("express/lib/response");
 const Character = require("../../models/Character");
 
 
-exports.getCharacters = (req, res)=>{
-    Character.findAll({
-        attributes: ['name', 'image']
-    })
-    res.send('received')
-}
+
+
+
+//Ruta para Consultar Personajes
+exports.getCharacters = async (req, res) => {
+  try {
+    let newCharacter = await Character.findAll();
+    res.json({
+      data: newCharacter,
+    });
+  } catch (error) {
+    res.json({
+      msg: "Error para mostrar datos" + error,
+      data: {},
+    });
+  }
+};
+
+
+//Ruta para crear Personajes
+exports.createCharacter = async (req, res) => {
+  try {
+    const { name, age, weight, history, image } = req.body;
+    let newCharacter = await Character.create({
+      name,
+      age,
+      weight,
+      history,
+      image,
+    });
+    if (newCharacter) {
+      res.json({
+        msg: "Usuario creado correctamente",
+        data: newCharacter,
+      });
+    }
+  } catch (error) {
+    res.json({
+      msg: "Error para crear Personaje " + error,
+      data: {},
+    });
+  }
+};
+
+
+//Consultar un solo personaje por Id
+exports.getOnCharacter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const character = await Character.findOne({
+      where: {
+        id
+      },
+    });
+    res.json({
+      data: character
+    });
+  /*   if(character.length > 0){
+        res.json({
+            msg: "id no existe"
+        })
+    } */
+    } catch (error) {
+    res.json({
+        msg: "id no existe " + error,
+        
+      });
+  }
+};
+
+//Eliminar un personaje por Id
+exports.deleteCharacter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    let deleteRowCount = await Character.destroy({
+      where: {
+        id,
+      },
+    });
+    res.json({
+      msg: "Personaje Eliminado Correctamente",
+      count: deleteRowCount,
+    });
+  } catch (error) {
+    res.json({
+      msg: "error para eliminar el personaje: " + error,
+    });
+  }
+};
+
+//Ruta para editar un personaje
+exports.updateCharacter = async (req, res) => {
+  const { id } = req.params;
+  const { name, age, weight, history, image } = req.body;
+  let character = await Character.findAll({
+      attributes: ["id", "name", "age", "weight", "history", "image"],
+    where: {
+      id
+    }
+  });
+  if (character.length > 0) {
+    character.forEach(async (c) => {
+      await c.update({
+        name,
+        age,
+        weight,
+        history,
+        image
+      });
+    });
+  }
+  return res.json({
+    msg: "Personaje Actualizado",
+    data: character
+  });
+};
--- routes/Character.js
+++ routes/Character.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { getCharacters } = require("../database/controllers/character.controller");
-
-
+const {
+  getCharacters,
+  createCharacter,
+  getOnCharacter,
+  updateCharacter,
+  deleteCharacter,
+} = require("../database/controllers/character.controller");
 
 //Ruta para mostrar personajes
 router.get("/characters", getCharacters);
 
+//Ruta para mostrar un personaje por Id
+router.get("/character/:id", getOnCharacter);
+
 //Ruta para crear Personaje
-/* router.post("/createcharacter", createCharacter);
+router.post("/createcharacter", createCharacter);
 
 //Ruta para Editar Personaje
-router.patch("/createcharacter", editCharacter);
+router.patch("/editcharacter/:id", updateCharacter);
 
 //Ruta para eliminar Personaje
-router.delete('/deletecharacter/:id', deleteCharacter) */
-
-
-
-
+router.delete("/deletecharacter/:id", deleteCharacter);
 
 module.exports = router;
